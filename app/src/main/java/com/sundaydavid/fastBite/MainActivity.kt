48776f85e141bc77e6d68c22758fda449d8f8534
sundaydavid <sundaydavid989@gmail.com>
2020-06-14 21:30:53 +0100

    added log for item destination

--- app/src/main/java/com/sundaydavid/fastBite/MainActivity.kt
+++ app/src/main/java/com/sundaydavid/fastBite/MainActivity.kt
@@ -2,11 +2,11 @@ package com.sundaydavid.fastBite
 
 import android.os.Bundle
 import android.util.Log
-import android.view.View
+
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
+
 import androidx.core.view.isVisible
-import androidx.drawerlayout.widget.DrawerLayout
+
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -37,8 +37,9 @@ class MainActivity : AppCompatActivity() {
     private fun setDestinationListener() {
         val navController =
             Navigation.findNavController(this, R.id.nav_host_fragment)
-        navController.addOnDestinationChangedListener { controller, destination, arguments ->
+        navController.addOnDestinationChangedListener { _, destination, _ ->
             val dest = resources.getResourceName(destination.id)
+            Log.d("Item destination", dest)
 
             when (destination.id) {
                 R.id.navigation_alphabet_detail,
