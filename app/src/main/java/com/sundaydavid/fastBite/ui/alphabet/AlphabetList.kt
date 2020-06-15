9efa63338dfb03cc368f6fa581ccaf8609007dc2
sundaydavid <sundaydavid989@gmail.com>
2020-06-15 10:56:37 +0100

    fixed minor bugs

--- app/src/main/java/com/sundaydavid/fastBite/ui/alphabet/AlphabetList.kt
+++ app/src/main/java/com/sundaydavid/fastBite/ui/alphabet/AlphabetList.kt
@@ -38,7 +38,6 @@ class AlphabetList : Fragment() {
         retainInstance = true
     }
 
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
